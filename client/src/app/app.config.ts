e60c030cea204c3d11726b06e793edaf6b838591
georgeibrahim1 <georgeibrahim213@gmail.com>
2026-07-15 09:44:35 +0300

    Add necessary package references, enhance app configuration, and improve UI components

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { errorInterceptor } from './services/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideRouter(routes)
   ]
 };
